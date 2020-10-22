7733b2ddfaf9fb05a8eb18f040e30d46442536c3
Ivan <frontenddeveloperweb@gmail.com>
2020-10-23 02:21:59 +0500

    Создана структура проекта, сверстаны компоненты, получение данных планет

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("hello world!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+ReactDOM.render(<App />,
+  document.getElementById('root'));
