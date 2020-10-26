58526098f88f7d0454cbbe7e76180af6534b930d
Ivan <frontenddeveloperweb@gmail.com>
2020-10-26 20:26:09 +0500

    Added loading and error message

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loading-box">
+            <div className="loadingio-spinner-dual-ring-8ldk0g2tj56"><div className="ldio-bdtrclzokm">
+            <div></div><div><div></div></div>
+            </div></div>
+        </div>
+    );
+};
+
+export default Spinner;
